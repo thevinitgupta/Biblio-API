6fafb810281165fedfe82d79128e7b7a9c1512f9
Vinit Gupta <thevinitgupta@gmail.com>
2024-04-06 19:59:48 +0530

    feature:add lombok library

--- src/main/java/tech/biblio/BookListing/entities/Comment.java
+++ src/main/java/tech/biblio/BookListing/entities/Comment.java
@@ -1,10 +1,12 @@
 package tech.biblio.BookListing.entities;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.util.Date;
 
+@Data
 public class Comment {
     @MongoId
     private ObjectId userId;
@@ -13,70 +15,11 @@ public class Comment {
     private Date createdAt;
     private Date updatedAt;
 
-    @Override
-    public String toString() {
-        return "Comment{" +
-                "userId='" + userId + '\'' +
-                ", username='" + username + '\'' +
-                ", content='" + content + '\'' +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                '}';
-    }
-
     public Comment(ObjectId userId, String username, String content, Date createdAt) {
         this.userId = userId;
         this.username = username;
         this.content = content;
         this.createdAt = createdAt;
         this.updatedAt = createdAt;
-    }
-
-    public Comment(ObjectId userId, String username, String content, Date createdAt, Date updatedAt) {
-        this.userId = userId;
-        this.username = username;
-        this.content = content;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public ObjectId getUserId() {
-        return userId;
-    }
-
-    public void setUserId(ObjectId userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
     }
 }
--- src/main/java/tech/biblio/BookListing/entities/Post.java
+++ src/main/java/tech/biblio/BookListing/entities/Post.java
@@ -1,5 +1,6 @@
 package tech.biblio.BookListing.entities;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Arrays;
 
 @Document("post")
+@Data
 public class Post {
     @Id
     private ObjectId id;
@@ -14,48 +16,4 @@ public class Post {
     private int likes;
     private String[] comments;
 
-    public Post() {
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", content='" + content + '\'' +
-                ", likes=" + likes +
-                ", comments=" + Arrays.toString(comments) +
-                '}';
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public int getLikes() {
-        return likes;
-    }
-
-    public void setLikes(int likes) {
-        this.likes = likes;
-    }
-
-    public String[] getComments() {
-        return comments;
-    }
-
-    public void setComments(String[] comments) {
-        this.comments = comments;
-    }
 }
