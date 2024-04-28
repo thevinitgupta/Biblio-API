17e04edee1d4904b33b2628cad70f3de7366c5d7
Vinit Gupta <thevinitgupta@gmail.com>
2024-04-28 11:46:43 +0530

    feature:add basic auth and public health check endpoint

---
+++ src/main/java/tech/biblio/BookListing/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package tech.biblio.BookListing.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+                requests.requestMatchers("/health").permitAll();
+                requests.anyRequest().authenticated();
+                });
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
