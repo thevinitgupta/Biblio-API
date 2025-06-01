5b35e09d2568381976ca7b56a4e337a5bf8c45c6
Vinit Gupta <thevinitgupta@gmail.com>
2025-06-01 12:20:15 +0530

    fix:remove env based keypair generation

---
+++ src/main/java/tech/biblio/BookListing/exceptions/KeyPairCreationException.java
@@ -0,0 +1,7 @@
+package tech.biblio.BookListing.exceptions;
+
+public class KeyPairCreationException extends RuntimeException {
+    public KeyPairCreationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/tech/biblio/BookListing/services/KeyService.java
+++ src/main/java/tech/biblio/BookListing/services/KeyService.java
@@ -2,23 +2,15 @@ package tech.biblio.BookListing.services;
 
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import tech.biblio.BookListing.exceptions.KeyPairCreationException;
 
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
+import java.security.*;
 import java.util.Base64;
 
 @Service
 public class KeyService {
-    @Value("${RSA_PUBLIC_KEY}")
-    private String publicKeyStr;
 
-    @Value("${RSA_PRIVATE_KEY}")
-    private String privateKeyStr;
 
     @Getter
     private PublicKey publicKey;
@@ -26,18 +18,45 @@ public class KeyService {
     private PrivateKey privateKey;
 
     @PostConstruct
-    public void init() throws Exception {
-        byte[] privateBytes = Base64.getDecoder().decode(privateKeyStr.replaceAll("\\s+", ""));
-        privateKey = KeyFactory.getInstance("RSA")
-                .generatePrivate(new PKCS8EncodedKeySpec(privateBytes));
+    public void init() throws KeyPairCreationException {
+        try {
+            generateNewKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            throw new KeyPairCreationException("Algorithm to create Key Pair not found");
+        }
+    }
 
+    private void generateNewKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator kpGenerator = KeyPairGenerator.getInstance("RSA");
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048); // Or 4096 for stronger security
+        KeyPair pair = generator.generateKeyPair();
 
-        byte[] publicBytes = Base64.getDecoder().decode(publicKeyStr.replaceAll("\\s+", ""));
-        publicKey = KeyFactory.getInstance("RSA")
-                .generatePublic(new X509EncodedKeySpec(publicBytes));
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
     }
 
     public String getPublicKeyBase64() {
-        return publicKeyStr;
+        return Base64.getEncoder().encodeToString(publicKey.getEncoded());
     }
+
+
+
+/*
+//    @PostConstruct
+//    public void init() throws Exception {
+//        byte[] privateBytes = Base64.getDecoder().decode(privateKeyStr.replaceAll("\\s+", ""));
+//        privateKey = KeyFactory.getInstance("RSA")
+//                .generatePrivate(new PKCS8EncodedKeySpec(privateBytes));
+//
+//
+//        byte[] publicBytes = Base64.getDecoder().decode(publicKeyStr.replaceAll("\\s+", ""));
+//        publicKey = KeyFactory.getInstance("RSA")
+//                .generatePublic(new X509EncodedKeySpec(publicBytes));
+//    }
+//
+//    public String getPublicKeyBase64() {
+//        return publicKeyStr;
+//    }
+ */
 }
